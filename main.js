d65e9553e3e0d4d67a66031978e5c977895e42a8
beansinmybutt <95232913+beansinmybutt@users.noreply.github.com>
2022-05-05 10:37:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.snake.more_menu = function() {
   
   // micro
   let img = new Image;
-  img.src = 'https://github.com/carlgustavh/GoogleSnakeCustomMenuStuffImages/blob/main/Micro.png?raw=true';
+  img.src = 'https://github.com/beansinmybutt/notmyrepsitory/blob/main/potato.jpeg';
   img.width = 47;
   img.height = 47;
   document.querySelector('#size').appendChild(img);
